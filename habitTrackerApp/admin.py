f74bc64be1f13681a1b7493e85193edf186c9a7e
azfarbkhr <azfarbkhr@gmail.com>
2023-04-23 19:11:46 +0500

    V 1.1, models added and admin registered

--- habitTrackerApp/admin.py
+++ habitTrackerApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Habit, Target, HabitLog
 
-# Register your models here.
+admin.site.register(Habit, 
+    list_display=['name', 'category', 'unit', 'user'],
+    list_filter=['category', 'user'],
+)
+
+admin.site.register(Target,
+    list_display=['habit', 'title', 'quantity', 'start_date', 'end_date', 'user'],
+    list_filter=['habit', 'start_date', 'end_date', 'user'],        
+)
+
+admin.site.register(HabitLog, 
+    list_display=['habit', 'date', 'quantity', 'user'],
+    list_filter=['habit', 'user'],
+)
--- habitTrackerApp/models.py
+++ habitTrackerApp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    unit = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta: 
+        verbose_name_plural = "Habits"
+
+    def __str__(self):
+        return self.name
+
+class Target(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    start_date = models.DateTimeField(default = timezone.now)
+    end_date = models.DateTimeField(default = timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateTimeField(default = timezone.now)
+    quantity = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.habit.name + " " + self.date.strftime("%m/%d/%Y")
